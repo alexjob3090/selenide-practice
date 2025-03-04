0186c9be8bb8905381f7858bc95e136c3b443993
AlexGithubovich <alexgithubovich@gmail.com>
2025-03-04 16:55:02 +0300

    add remote selenoid

--- src/test/java/com/example/google/GoogleTest.java
+++ src/test/java/com/example/google/GoogleTest.java
@@ -1,5 +1,6 @@
 package com.example.google;
 
+import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -9,6 +10,13 @@ import org.example.pages.GooglePage;
 import org.example.pages.GoogleResultsPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,12 +28,38 @@ public class GoogleTest {
     private GoogleResultsPage googleResultsPage;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws MalformedURLException {
 //        if (WebDriverRunner.hasWebDriverStarted()) {
 //            closeWebDriver();
 //        }
         Configuration.baseUrl = "https://duckduckgo.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserVersion", "100.0");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Test badge...");
+
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+        }});
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub"), options);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         googlePage = new GooglePage();
         googleResultsPage = new GoogleResultsPage();
